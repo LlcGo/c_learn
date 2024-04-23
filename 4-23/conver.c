4809d55f60618865d431ecfbcbe2cb1f4c93751f
k71625 <31415926lc>
2024-04-23 15:42:35 +0800

    整形提升

--- 4-23/conver.c
+++ 4-23/conver.c
@@ -7,5 +7,12 @@ int main(void)
   // 先转后除
   mean = (double)num / count;
   printf("强转后的mean->%f\n",mean);
+
+  // 整数提升
+  char c = 'c';
+  printf("c=%d\n",c);
+  int cp = c + num;
+  printf("num=%d\n",num);
+  printf("cp=%d\n",cp);
   return 0;
 }
