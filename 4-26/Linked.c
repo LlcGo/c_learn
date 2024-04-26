8ee7e4253ac7faa94a1cdbec93b774cb447e40b1
k71625 <31415926lc>
2024-04-26 19:45:54 +0800

    链表

---
+++ 4-26/Linked.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Book{
+   char title[20];
+   char author[20];
+   struct Book * book;
+};
+
+static struct  Book * tail;
+
+void createBook(struct Book *book)
+{
+  printf("请输入书名:");
+  scanf("%s",book->title);
+  printf("请输入作者:");
+  scanf("%s",book->author);
+} 
+
+void add(struct Book **book)
+{
+    struct Book * newbook,*temp;
+    newbook = (struct Book *)malloc(sizeof(struct Book));
+    if(newbook == NULL)
+    {
+      printf("内存空间不足");
+      exit(1);
+    }
+    
+    createBook(newbook);
+    if(*book == NULL)
+    {
+       *book = newbook;
+        newbook->book = NULL; 
+     }else{
+       tail->book = newbook;
+       newbook->book = NULL;
+     }  
+    tail = newbook; 
+      
+}
+
+void printfBook(struct Book * book)
+{
+   struct Book * bk;
+   int count = 1;
+   bk = book;
+   while(bk != NULL)
+   {
+     printf("Book%d",count);
+     printf("书名:%s,作者:%s\n",bk->title,bk->author);
+     bk = bk->book;
+     count++;      
+   }  
+}
+
+void freeBook(struct Book ** book)
+{
+  struct Book *temp;
+  while(*book != NULL)
+  {
+    temp = *book;
+    *book = (*book)->book;
+    free(temp);
+  }    
+}
+
+int main()
+{
+  struct Book * book = NULL;
+  char c;
+  while(1){
+     printf("是否需要添加书籍(Y/N)");
+   do{
+      c = getchar();
+    }while(c != 'Y' && c != 'N');
+    if(c == 'Y')
+    {
+      add(&book);
+    } else{
+      break;
+    }
+  }
+    printf("是否需要打印数据(Y/N)");
+   do{
+     c = getchar();
+    }while(c != 'Y' && c != 'N');
+    if(c == 'Y'){
+     printfBook(book);
+    }
+   freeBook(&book);
+  return 0;
+}
