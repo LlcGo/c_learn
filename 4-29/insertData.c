8abc6625d9dedd804e7e497e95479f636845edbb
k71625 <31415926lc>
2024-04-29 16:12:05 +0800

    插入优化

--- 4-29/insertData.c
+++ 4-29/insertData.c
@@ -14,7 +14,7 @@ int main(void)
 }
 
 
-// 为了方便再插入头节点的时候操作所以使用指向头节点指针的指针
+// 需要传入指向头节点的指针的指针，才可以真正操控头节点 如果只是传入 Node * rootp的,函数内无法访问root这个节点
 int insert(Node **rootp, int value)
 {
       Node * node;
@@ -42,4 +42,24 @@ int insert(Node **rootp, int value)
       pre->next = new;
       }
       return 1;         
+}
+
+// 最终优化
+int goodInstert(Node **linkp,int value)
+{
+    register Node * current;
+    register Node * new;
+    
+    while((current = *linkp) != NULL && current->value <value)
+    {
+        linkp = &current->next;        
+    }
+    new = (Node*)malloc(sizeof(Node*));
+    if(new == NULL)
+    {
+       return 0;
+    }
+    new->value = value;
+    new->link = current;  
+    *linkp = new;      
 }
