f262e463efeb914284f896690f36d7cfad393604
k71625 <1641767724@qq.com>
2024-05-04 10:54:00 +0800

    2024-05-04

--- 5-4/stack0.c
+++ 5-4/stack0.c
@@ -27,14 +27,14 @@ int main(void)
 
 }
 
-
+// 入栈
 int push(Stack * s, int e)
 {
   // 俩个数组指针相减，获得此类型个数
   // 如果栈满，追加空间
   if(s->top - s->base >= stackSize)
   {
-    // 每次扩容 stackSize个
+    // 每次扩容 stackSize个 使用malloc进行拷贝的
     s->base = (ElemType *)realloc(s->base,(s->stackSize + SIZE) * sizeof(ElemType)); 
     // top 往上移动 stackSize的空间
     s->top = s->base + s->stackSize;
@@ -45,4 +45,14 @@ int push(Stack * s, int e)
   //上移一位
   s->top++;
   return 0;   
+}
+
+// 出栈
+int pop(Stack * s)
+{
+   if(s->base == s->top)
+       return -1; 
+   // 指针下移一位后才能获得数据
+   int i = *--(s->top); 
+   return i; 
 }
