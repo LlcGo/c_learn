6eb2c3b17c76aa14398c32e553fe7ba10253bebf
k71625 <1641767724@qq.com>
2024-05-07 11:15:21 +0800

    2024-05-07

--- 5-7/stack.c
+++ 5-7/stack.c
@@ -15,4 +15,18 @@ int IsEmpty(stack s)
   return s->next == NULL;
 }
 
+stack createStack(void)
+{
+  stack s = (stack)malloc(sizeof(Node));
+  s->next = NULL;
+  return s; 
+}
 
+
+void push(stack s,ElementType e)
+{ 
+   stack new = (stack *)malloc(sizeof(Node));
+   new->element = e;
+   new->next = s->next;
+   s->next= new;
+}
