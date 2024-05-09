b5719523b94b29c7f6a728545ce3feab60f983bf
k71625 <1641767724@qq.com>
2024-05-09 15:03:04 +0800

    2024-05-09

---
+++ 5-9/hash2.c
@@ -0,0 +1,21 @@
+igned int index;
+typedef Index Position;
+
+struct HashTbl;
+typedef struct HashTbl *HashTable;
+
+enum KindOfEntry{Legitimate,Empty,Delete};
+
+struct HashEntry
+{
+	ElementType Element;
+	enum KindOfEntry;
+};
+
+typedef struct HashEntry Cell;
+
+struct HashTbl
+{
+	int TableSize;
+	Cell *TheCells;
+};
