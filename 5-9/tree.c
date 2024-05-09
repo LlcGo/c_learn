47ab10f2da4ba2ab3e4f85e1aa4ae6183d5b52e2
k71625 <1641767724@qq.com>
2024-05-09 23:39:21 +0800

    2024-05-09

--- 5-9/tree.c
+++ 5-9/tree.c
@@ -4,6 +4,21 @@ typedef struct BitNode
 {
   ElemType data;
   struct BitNode * lchild, *rchild;
-}BitNode,*BiTree;
+}BitNode,*BitTree;
 
-
+void createBitTree(BitTree * T)
+{
+  char c;
+  scanf("%c",&c);
+  if(' ' == c)
+  {
+     *T = NULL;
+  }
+  else
+  {
+     *T = (BitTree)malloc(sizeof(BitNode));
+     (*T)->data = c;
+     createBitTree(&(*T)->lchild);
+     createBitTree(&(*T)->rchild);
+  }
+}
