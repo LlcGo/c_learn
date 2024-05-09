d693f55640bf8b31c3f02bcefeecd4bf0d324fb7
k71625 <1641767724@qq.com>
2024-05-09 14:23:57 +0800

    2024-05-09

--- 5-9/hash.c
+++ 5-9/hash.c
@@ -2,6 +2,26 @@
 
 typedef unsigned int Index;
 
+
+HashTable init(int tableSize)
+{
+		HashTable H;
+		int i;
+		
+		// 创建整个HashTable
+		H = (HashTable)malloc(sizeof(struct HashTbl));
+		H->tableSize = tableSize;
+		// 创建HashTable 里面的 链表
+		H->theLists = (List)malloc(H->tableSize * sizeof(ListNode));
+		for(i = 0; i < H->tableSize; i++)
+		{
+		    H->theLists[i] =  malloc(sizeof(ListNode));
+	    	H->theLists[i]->next = NULL;
+		}	
+		return H;
+}
+
+
 Index hash(const char *key, int tableSize)
 {
    unsigned int hashval = 0;
