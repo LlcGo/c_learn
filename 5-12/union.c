b6136b3c7164530da8d8a53a56c427c2f172e76c
k71625 <1641767724@qq.com>
2024-05-12 14:30:02 +0800

    2024-05-12

--- 5-12/union.c
+++ 5-12/union.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+#define NumSets 10
+
 typedef int DisJSet[NumSets + 1];
 typedef int SetType;
 typedef int ElementType;
 
 
+void init(DsiJSet S)
+{
+  int i;
+  for( i = NumSets; i > 0; i-- )
+  {
+     s[i] = 0;
+  }
+}
