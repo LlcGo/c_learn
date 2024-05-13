5cb2e82943335242eb05c12cb8590ecdb02b5999
k71625 <1641767724@qq.com>
2024-05-13 13:58:06 +0800

    2024-05-13

--- 5-13/tree.c
+++ 5-13/tree.c
@@ -5,9 +5,24 @@ typedef char ElemType;
 
 typedef struct BiTNode
 {
-   char datal
+   char data;
    struct BiTNode *lchild,*rchild;
 }BiTNode,*BiTree;
 
 
-
+void createTree(BiTree *T)
+{
+  char c;
+  scanf("%c",&c);
+  if(' ' == c)
+  {
+     *T = NULL;
+  }
+  else
+  {
+     *T = (BiTree)malloc(sizeof(BiTNode));
+     *T->data = c;
+     createTree(&(*T)->lchild);
+     createTree(&(*T)->rchild);
+  }
+}
