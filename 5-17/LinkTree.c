3dfc91b2d147b04344ed754000404e312fb69ea7
k71625 <1641767724@qq.com>
2024-05-17 14:44:13 +0800

    2024-05-17

--- 5-17/LinkTree.c
+++ 5-17/LinkTree.c
@@ -80,3 +80,42 @@ int BiTreeDepth(BiTree T)
   }
   return i > j ? i+1 : j+1;
 }
+
+// 返回跟根
+Element Root(BiTree T)
+{
+  if(!BiTreeIsEmpty(T)) return T->data;
+  else return 0;
+}
+
+void PreOrderTraverse(BiTree T)
+{ 
+	if(T==NULL)
+		return;
+	printf("%c",T->data);/* 显示结点数据，可以更改为其它对结点操作 */
+	PreOrderTraverse(T->lchild); /* 再先序遍历左子树 */
+	PreOrderTraverse(T->rchild); /* 最后先序遍历右子树 */
+}
+
+/* 初始条件: 二叉树T存在 */
+/* 操作结果: 中序递归遍历T */
+void InOrderTraverse(BiTree T)
+{ 
+	if(T==NULL)
+		return;
+	InOrderTraverse(T->lchild); /* 中序遍历左子树 */
+	printf("%c",T->data);/* 显示结点数据，可以更改为其它对结点操作 */
+	InOrderTraverse(T->rchild); /* 最后中序遍历右子树 */
+}
+
+/* 初始条件: 二叉树T存在 */
+/* 操作结果: 后序递归遍历T */
+void PostOrderTraverse(BiTree T)
+{
+	if(T==NULL)
+		return;
+	PostOrderTraverse(T->lchild); /* 先后序遍历左子树  */
+	PostOrderTraverse(T->rchild); /* 再后序遍历右子树  */
+	printf("%c",T->data);/* 显示结点数据，可以更改为其它对结点操作 */
+}
+
