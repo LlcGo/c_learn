a90b5455c5a8b0310d10edc32d9b237752d10427
k71625 <1641767724@qq.com>
2024-05-20 16:46:53 +0800

    2024-05-20

--- 5-20/AVLTree.c
+++ 5-20/AVLTree.c
@@ -25,5 +25,24 @@ int getTreeHeigh(AVLTree T)
    return T->height;
 }
 
+int max(int a,int b)
+{
+  return (a>b)?a:b;
+}
 
-
+// 左旋
+AVLTree leftRoat(AVLTree *T)
+{
+   // 新的根是当前根的右结点
+   AVLTree newRoot = T->rchild;
+   // 如果当前旋转的新根有左子树，左子树挂到之前根的右子树
+   AVLTree newRootLeft = T->lchild;
+   // 新的左结点是之前的根
+   newRoot->left = T;
+   T->rchild = newRootLeft;
+   // 更新树高
+   newRoot->height = 1 + max(getTreeHeigh(newRoot->lchid),getTreeHeigh(newRoot->rchild));
+   T->height = 1 + max(getTreeHeigh(T->lchid),getTreeHeigh(T->rchild));
+   return newRoot;
+}
+// 右旋
