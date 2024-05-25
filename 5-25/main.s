b05db6367aa409b025827aeba7a39a06f3734274
k71625 <1641767724@qq.com>
2024-05-25 19:35:13 +0800

    2024-05-25

--- 5-25/main.s
+++ 5-25/main.s
@@ -1,9 +1,30 @@
 	.file	"main.c"
 	.text
+	.globl	f
+	.type	f, @function
+f:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	f, .-f
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB1:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
@@ -17,7 +38,7 @@ main:
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE1:
 	.size	main, .-main
 	.ident	"GCC: (GNU) 7.3.1 20180303 (Red Hat 7.3.1-5)"
 	.section	.note.GNU-stack,"",@progbits
