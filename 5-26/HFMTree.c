e3824c713c8afce2273f8da52c66ac459895b73c
k71625 <1641767724@qq.com>
2024-05-26 15:14:42 +0800

    2024-05-26

--- 5-26/HFMTree.c
+++ 5-26/HFMTree.c
@@ -21,4 +21,28 @@ void printHuffmancode(HuffmanTree ht,int n,char *pwd);// 先序打印
 
 void HuffmanDecoding(HuffmanTree ht,int *m1,int *m2);// 在生成的哈夫曼编码中查询目标
 
+Select(HuffmanTree ht,int j,int *m1,int *m2)
+{
+   double min1 = 999999999;
+   double min2 = 999999999;
+   int j;
+   for( j = 1; j <= x; j++)
+  {
+     if(ht[j].weight < min1 && ht[j].parent == 0)
+     { 
+         min1 = ht[j].weight;
+         *m1 = j; 
+     }
+  }
+  
+  int k;
+  for( k = 1; k <= x; k++)
+  {
+     if(ht[k].weight < min2 && k !=*m1 && ht[k].parent == 0)
+     {
+        min2 = ht[k].weight;
+        *m2 = k;
+     }
+  }
+}
 
