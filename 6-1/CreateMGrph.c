302a69b3f8afbbcc323570d9cd97b4f260c3f642
k71625 <1641767724@qq.com>
2024-06-01 16:05:32 +0800

    2024-06-01

--- 6-1/CreateMGrph.c
+++ 6-1/CreateMGrph.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define MAXVEX 100  /**/
-#define GRAPH_INFINITY 65535 
+#define MAXVEX 100  /*矩阵多大*/
+#define GRAPH_INFINITY 65535 /*无穷大*/
 
-typedef char VertexType;
-typedef int EdgeType;
+typedef char VertexType; /*边类型*/
+typedef int EdgeType;  /*矩阵权值类型*/
 
 typedef struct
 {
@@ -12,4 +12,23 @@ typedef struct
    int numNodes,numEdges;
 }MGraph;
 
+
+void CreateMGraph(MGraph *G)
+{
+   int i,j,k,w;
+   printf("请输入顶点数和边数:\n");
+   scanf("%d,%d",&G->numNodes,&G->numEdges);
+   for(i = 0; i < G->numNodes; i++) // 初始化顶点表
+	   scanf(&G->vexs[i]);
+   for(i = 0;i< G->numNodes;i++)
+	   for(j = 0; j < G->numNodes;j++)
+		   G->arc[i][j] = GRAPH_INFINITY; /*初始化矩阵*/
+   for(k = 0; k < G->numNodes; k++)
+   {
+	   printf("请输入(vi vj)上的下表i, 下标j和权w:\n");
+	   scanf("%d,%d,%d",&i,&j,&w);
+	   G->arc[i][j] = w;
+	   G->arc[j][i] = G->arc[i][j]; /*无向图矩阵对称*/
+   }
+}
 
