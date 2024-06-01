fcae9f800e793b8e165eb48bcfd830fd86a41ae6
k71625 <1641767724@qq.com>
2024-06-01 15:52:38 +0800

    2024-06-01

--- 6-1/CreateMGrph.c
+++ 6-1/CreateMGrph.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#define MAXVEX 100  /**/
+#define GRAPH_INFINITY 65535 
+
+typedef char VertexType;
+typedef int EdgeType;
+
+typedef struct
+{
+   VertexType vexs[MAXVEX];
+   EdgeType arc[MAXVEX][MAXVEX];
+   int numNodes,numEdges;
+}MGraph;
+
 
