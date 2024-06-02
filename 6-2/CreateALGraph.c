ff444e052eb30ca35c4ffe3c81985875e3666026
k71625 <1641767724@qq.com>
2024-06-02 15:26:58 +0800

    2024-06-02

--- 6-2/CreateALGraph.c
+++ 6-2/CreateALGraph.c
@@ -26,4 +26,33 @@ typedef struct
 	int numNodes,numEdges; /*图中当前结点数和边数*/
 }
 
+void CreateALGraph(GraphAdjList *G)
+{
+	int i,j,k;
+	EdgNode *e;
+	printf("请输入顶点数和边数:\n");
+	scanf("%d,%d",&G->numNodes,&G->numEdges);
+	for(i = 0; i < G->numNodes; i++)
+	{
+		scanf(&G->adjList[i].data);
+		G->adjList[i].firstedge = NULL;
+	}
+
+	for(k = 0; k < G->numEdges; k++)
+	{
+		printf("输入边(vi,vj)上的顶点序号:\n");
+		scanf("%d,%d",&i,&j);
+		e = (EdgeNode *)malloc(sizeof(EdgeNode));
+		e->adjvex = j;
+		e->next = G->adjList[i].firstedge;
+		G->adjList[i].firstedge = e;
+
+		e = (EdgeNode *)malloc(sizeof(EdgeNode));
+		e->adjvex = i;
+		e->next = G->adjList[j].firstedge;
+		G->adjList[j].firstedge=e;
+	}
+}
+
+
 
