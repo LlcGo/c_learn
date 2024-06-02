20258fcf70cb268a84a21c8706420f2d9f725e71
k71625 <1641767724@qq.com>
2024-06-02 10:12:06 +0800

    2024-06-02

--- sy0/ics2023/nemu/src/monitor/sdb/sdb.c
+++ sy0/ics2023/nemu/src/monitor/sdb/sdb.c
@@ -49,7 +49,13 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  printf("args->%c\n",*args);	
   return -1;
+}
+
+/*调试*/
+static int cmd_si(char * args){
+	  return 0;
 }
 
 static int cmd_help(char *args);
@@ -62,6 +68,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Debug", cmd_si},
 
   /* TODO: Add more commands */
 
