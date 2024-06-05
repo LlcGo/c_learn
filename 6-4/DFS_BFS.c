9bc2b788fc015b010e451c032f85b08c575031a9
k71625 <1641767724@qq.com>
2024-06-05 16:14:55 +0800

    2024-06-05

--- 6-4/DFS_BFS.c
+++ 6-4/DFS_BFS.c
@@ -144,4 +144,35 @@ void DFSTraverse(MGraph G)
 		   DFS(G,i);
 }
 
+/*广度优先遍历*/
+void BFSTraverse(MGraph G)
+{
+	int i, j;
+	Queue Q;
+	for(i = 0; i < G.numVertexes; i++)
+		visited[i] = FALSE;
+	InitQueue(&Q);
+        for(i = 0; i < G.numVertexes; i++)
+	{
+		if(!visited[i])
+		{
+			visitied[i] = TRUE;
+			printf("%c ",G.vexs[i]);
+		}
+		EnQueue(&Q,i);
+		while(!QueueEmpty(Q))
+		{
+			DeQueue(&Q,&i);
+			for(j = 0; j < G.numVertexes; j++)
+			{
+				if(G.arc[i][j] == 1 && !visited[j])
+				{
+					visited[j] = TRUE;
+					printf("%c ",G.vexs[j]);
+					EnQueue(&Q,j);
+				}
+			}
+		}
+	}
 
+}
