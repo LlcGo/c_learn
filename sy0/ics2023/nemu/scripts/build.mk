39afc921e6be2db11c558f0779e1c1c6d77fc2a0
tracer-ics2023 <tracer@njuics.org>
2024-06-06 15:38:41 +0800

    >  compile NEMU 221220000 张三 Linux root0-virtual-machine 5.15.0-107-generic #117~20.04.1-Ubuntu SMP Tue Apr 30 10:35:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:38:41 up  1:49, 10 users,  load average: 0.00, 0.02, 0.03

--- sy0/ics2023/nemu/scripts/build.mk
+++ sy0/ics2023/nemu/scripts/build.mk
@@ -32,12 +32,16 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
+	@$(CC) $(CFLAGS) -E -MF /dev/null $< | grep -ve '^#' | \
+		clang-format - > $(basename $@).i
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
 	@echo + CXX $<
 	@mkdir -p $(dir $@)
 	@$(CXX) $(CFLAGS) $(CXXFLAGS) -c -o $@ $<
+	@$(CC) $(CFLAGS) -E -MF /dev/null $< | grep -ve '^#' | \
+                clang-format - > $(basename $@).i
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 # Depencies
