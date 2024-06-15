d1a0039dc6eb534124fe857ffcac30d4d5bcf2f1
tracer-ics2023 <tracer@njuics.org>
2024-06-15 22:52:39 +0800

    >  compile NEMU 221220000 张三 Linux root0-virtual-machine 5.15.0-107-generic #117~20.04.1-Ubuntu SMP Tue Apr 30 10:35:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:52:39 up 1 day,  5:55, 12 users,  load average: 0.03, 0.02, 0.00

--- sy/ics2023/nemu/src/isa/riscv32/inst.c
+++ sy/ics2023/nemu/src/isa/riscv32/inst.c
@@ -21,7 +21,7 @@
 #define R(i) gpr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
-
+	 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
   TYPE_N, TYPE_J, TYPE_B,// none
@@ -32,7 +32,14 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do{ *imm= SEXT((BITS(i,31,31)<<20)+(BITS(i,19,12)<<12)+(BITS(i,20,20)<<11) +(BITS(i,30,21)<<1),21);  }while(0)
+//#define immJ() do{ *imm= SEXT((BITS(i,31,31)<<20)+(BITS(i,19,12)<<12)+(BITS(i,20,20)<<11) +(BITS(i,30,21)<<1),21);  }while(0)
+#define immJ() do { *imm = SEXT(( \
+(BITS(i, 31, 31) << 19) | \
+BITS(i, 30, 21) | \
+(BITS(i, 20, 20) << 10) | \
+(BITS(i, 19, 12) << 11) \
+) << 1, 21); } while(0)
+
 #define immB() do{ *imm= SEXT((BITS(i,31,31)<<12)+(BITS(i,30,25)<<5),7)| SEXT( (BITS(i,11,8)<<1)  + (BITS(i,7,6)<< 11), 6);}while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -75,7 +82,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(rd) = Mr(src1 + imm,4));
   INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, R(rd) = (src1 < imm) ? 1 : 0);
   
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , I, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
