8cfa82a83bff8f8fc21a5a51e404b040488da401
tracer-ics2023 <tracer@njuics.org>
2024-06-16 16:03:33 +0800

    >  compile NEMU 221220000 张三 Linux root0-virtual-machine 5.15.0-107-generic #117~20.04.1-Ubuntu SMP Tue Apr 30 10:35:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:03:33 up 1 day, 11:17, 15 users,  load average: 0.11, 0.05, 0.01

--- sy/ics2023/nemu/src/utils/itrace.c
+++ sy/ics2023/nemu/src/utils/itrace.c
@@ -11,6 +11,17 @@ ItraceNode iringbuf[MAX_IRINGBUF];
 int p_cur = 0;
 bool full =false;
 
+/**
+typedef struct {
+	char name[32]; // func name, 32 should be enough
+	paddr_t addr;
+	unsigned char info;
+	Elf64_Xword size;
+} SymEntry;
+
+SymEntry *symbol_tbl = NULL;
+int call_depth = 0;
+**/
 extern void trace_inst(word_t pc,uint32_t inst){
    iringbuf[p_cur].pc = pc;
    iringbuf[p_cur].inst = inst;
@@ -46,3 +57,34 @@ extern void display_inst(){
 	}while((i = (i+1) % MAX_IRINGBUF) != end );
  	puts(ANSI_NONE);
 }
+/**
+extern void trace_func_call(paddr_t pc, paddr_t target){
+   if(symbol_tbl == NULL) return;
+   ++call_depth;
+   
+   if(call_depth <= 2) return;
+
+   int i = find_symbol_func(target,true);
+   ftrace_write(FMT_PADDR ": %s*scall [%s@" FMT_PADDR "]\n",
+		pc,
+		(call_depth-3)*2, "",
+		i >= 0? symbol_tbl[i].name:"???",
+		target
+    );
+}
+
+void trace_func_ret(paddr_t pc){
+	if(symbol_tbl == NULL) return;
+	if(call_depth <= 2) return;
+
+	int i = find_symbol_func(pc,false);
+	ftrace_write(FMT_PADDR ": %*sret [%s]\n",
+	pc,
+	(call_depth-3)*2,"",
+        i>0?symbol_tbl[i].name:"???"
+	);
+
+	--call_depth;
+}
+**/
+
