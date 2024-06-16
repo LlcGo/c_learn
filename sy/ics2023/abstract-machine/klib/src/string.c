5cbff65e523645be9c7d54a0ce746561f13ff134
tracer-ics2023 <tracer@njuics.org>
2024-06-16 23:16:02 +0800

    >  compile NEMU 221220000 张三 Linux root0-virtual-machine 5.15.0-107-generic #117~20.04.1-Ubuntu SMP Tue Apr 30 10:35:57 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:16:02 up 1 day, 17:11, 13 users,  load average: 0.23, 0.07, 0.01

--- sy/ics2023/abstract-machine/klib/src/string.c
+++ sy/ics2023/abstract-machine/klib/src/string.c
@@ -53,16 +53,14 @@ int strcmp(const char *s1, const char *s2) {
      const char *s1p = s1;
      const char *s2p = s2;
     // if(s1len != s2len) return 1;
-     while((*s1p) != '\0')
+     while((*s1p) != '\0' && (*s1p == *s2p))
      {
-	  if((*s1p)!=(*s2p))
-	  {
-		  return -1;
-	  }	  
 	  s1p++;
 	  s2p++;
      }
-     return 0;
+     int t;
+     t = *s1p - *s2p;
+     return t;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
